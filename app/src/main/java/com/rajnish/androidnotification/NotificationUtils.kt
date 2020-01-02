7430c8de17db19caea4e68ea419c9cdb45fdd9cf
Rajnish23 <rajnishsuryavanshi223@gmail.com>
2020-01-02 22:22:43 +0300

    Added notification for friend request

--- app/src/main/java/com/rajnish/androidnotification/NotificationUtils.kt
+++ app/src/main/java/com/rajnish/androidnotification/NotificationUtils.kt
@@ -1,10 +1,29 @@
 package com.rajnish.androidnotification
 
 import android.app.NotificationManager
+import android.content.Context
+import androidx.core.app.NotificationCompat
 
 private val NOTIFICATION_ID = 100;
 
-fun NotificationManager.sendNotification(title : String , message : String){
+fun NotificationManager.sendNotification(
+    title: String,
+    message: String,
+    channel: String,
+    applicationContext: Context
+) {
+
+    //TODO("Create Notification")
+    val builder = NotificationCompat.Builder(
+        applicationContext,
+        channel
+    )
+        .setSmallIcon(R.mipmap.ic_launcher)
+        .setContentTitle(title)
+        .setContentText(message)
+
+    notify(NOTIFICATION_ID, builder.build())
+
 
 }
 
