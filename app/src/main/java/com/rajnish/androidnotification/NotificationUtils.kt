dcd27877bc031ad0b400f7e6aeccdbe08fb7470a
Rajnish23 <rajnishsuryavanshi223@gmail.com>
2020-01-02 21:38:42 +0300

    Added notification util method

---
+++ app/src/main/java/com/rajnish/androidnotification/NotificationUtils.kt
@@ -0,0 +1,11 @@
+package com.rajnish.androidnotification
+
+import android.app.NotificationManager
+
+private val NOTIFICATION_ID = 100;
+
+fun NotificationManager.sendNotification(title : String , message : String){
+
+}
+
+fun NotificationManager.cancelNotification() = cancelAll()
